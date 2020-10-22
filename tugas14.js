36cd508b094a7581a8a7d0f796143e0a330cbdf3
bayuik <bayuindrakusuma05@gmail.com>
2020-10-22 21:49:49 +0700

    using Math methods round, ceil, floor

---
+++ tugas14.js
@@ -0,0 +1,13 @@
+// Teman teman buatlah sebuah variabel untuk melakukan operasi pembagian , teman teman lakukan pembagian 37 dibagi tanggal lahir teman teman.
+// Setelah itu teman teman tampilkan hasil pembagian tersebut.
+// Setelah itu teman teman tampilkan kembali hasilnya dengan perintah math round , ceil dan floor.
+
+let divide = (age) => {
+    return 37 / age;
+}
+let func = divide(19);
+
+console.log(func);
+console.log(Math.round(func));
+console.log(Math.ceil(func));
+console.log(Math.floor(func));

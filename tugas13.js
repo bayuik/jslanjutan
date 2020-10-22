3d072eec4fc303a0808f70bb22b7084c3d96f111
bayuik <bayuindrakusuma05@gmail.com>
2020-10-22 21:40:58 +0700

    using the unshift methods

---
+++ tugas13.js
@@ -0,0 +1,7 @@
+// Teman teman buatlah sebuah variabel array dengan 2 buah nama buah buahan pisang , jeruk
+// Setelah itu teman teman tampilkan nilai awal array tersebut, lakukan perintah unshift dan tambahkan data  apel dan mangga
+
+let fruit = ['pisang', 'jeruk'];
+console.log(fruit);
+fruit.unshift('apel', 'mangga');
+console.log(fruit);

927c10f9eff3710e576bb1b06bd5bcdb3994fc00
bayuik <bayuindrakusuma05@gmail.com>
2020-10-22 21:32:40 +0700

    using the shift pop methods

---
+++ tugas12.js
@@ -0,0 +1,9 @@
+// dengan 4 buah nama buah buahan pisang , jeruk , apel dan mangga
+// Setelah itu teman teman tampilkan nilai awal array tersebut, lakukan perintah pop dan shift
+
+let fruit = ['pisang', 'jeruk', 'apel', 'mangga'];
+console.log(fruit);
+fruit.pop();
+console.log(fruit);
+fruit.shift();
+console.log(fruit);

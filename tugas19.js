e067fda49efd8c4bfb415e552f92c1c41dfa93ba
bayuik <bayuindrakusuma05@gmail.com>
2020-10-23 22:17:20 +0700

    using string methods repeat

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+let repeat = (a, b) => {
+    console.log(a.repeat(5));
+
+    console.log(b);
+    console.log(b.repeat(4));
+    console.log(b.repeat(7));
+    console.log(b.repeat(9));
+    console.log(b.repeat(11));
+    console.log(b.repeat(9));
+    console.log(b.repeat(7));
+    console.log(b.repeat(4));
+    console.log(b);
+
+}
+
+repeat('Semangat.....', '*****');

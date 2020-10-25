23045cea729e5c5b36fae649680e2581bef50ac9
bayuik <bayuindrakusuma05@gmail.com>
2020-10-25 21:48:47 +0700

    reverse word using split, reverse and join

---
+++ reverseWord.js
@@ -0,0 +1,8 @@
+let reverseWord = word => {
+    let data = word.split("").reverse().join("");
+    console.log(data);
+}
+
+reverseWord("Javascript");
+reverseWord("alohahola");
+reverseWord("Jawa_timur");
